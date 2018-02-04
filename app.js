6e455d32d30771c13f11d80a03c9f0449b750890
bushblade <bushblade@gmail.com>
2018-02-04 11:56:30 +0000

    first refactor

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-const intervalBar = document.getElementById('interval-bar'),
-  breakBar = document.getElementById('break-bar'),
-  intTime = document.getElementById('int-time'),
-  breakTime = document.getElementById('break-time'),
-  sm = document.getElementById('sm'),
-  sp = document.getElementById('sp'),
-  bm = document.getElementById('bm'),
-  bp = document.getElementById('bp'),
-  start = document.getElementById('start'),
+const start = document.getElementById('start'),
   stop = document.getElementById('stop'),
   allBtns = Array.from(document.querySelectorAll('button'));
 
-let intMinutes = 5,
-  intSeconds = 0,
-  breakMinutes = 1,
-  breakSeconds = 0,
-  timerCount = 2,
-  interval;
+const sessionTimer = {
+  minutes: 5,
+  seconds: 0,
+  display: document.getElementById('int-time'),
+  bar: document.getElementById('interval-bar')
+}
 
-sm.addEventListener('click', intImeMinus);
-sp.addEventListener('click', intTimePlus);
-bm.addEventListener('click', breakMinus);
-bp.addEventListener('click', breakPlus);
+const breakTimer = {
+  minutes: 5,
+  seconds: 0,
+  display: document.getElementById('break-time'),
+  bar: document.getElementById('break-bar')
+}
 
+let interval,
+  timerCount = 2;
+
+sm.addEventListener('click', () => minus(sessionTimer));
+sp.addEventListener('click', () => plus(sessionTimer));
+bm.addEventListener('click', () => minus(breakTimer));
+bp.addEventListener('click', () => plus(breakTimer));
 start.addEventListener('click', function () {
-  timer(intMinutes, intTime, intervalBar, interval);
+  timer(sessionTimer);
   toggleBtns();
 });
-
 stop.addEventListener('click', stopAll)
 
 function stopAll() {
   stopInterval();
   clearInterval(interval);
   clearInterval(1);
-  intTime.textContent = `${minTwoDidgets(intMinutes)}:${minTwoDidgets(intSeconds)}`;
-  breakTime.textContent = `${minTwoDidgets(breakMinutes)}:${minTwoDidgets(breakSeconds)}`;
-  intervalBar.value = 0;
-  breakBar.value = 0;
+  sessionTimer.display.textContent = `${minTwoDidgets(sessionTimer.minutes)}:${minTwoDidgets(sessionTimer.seconds)}`;
+  breakTimer.display.textContent = `${minTwoDidgets(breakTimer.minutes)}:${minTwoDidgets(breakTimer.seconds)}`;
+  sessionTimer.bar.value = 0;
+  breakTimer.bar.value = 0;
   toggleBtns();
   timerCount = 2;
 }
@@ -47,45 +47,28 @@ function stopInterval() {
     clearInterval(i);
 }
 
-function toggleBtns() {
-  allBtns.forEach(x => x.classList.toggle('is-hidden'));
-}
-
-//interval timings
-function intImeMinus() {
-  if (intMinutes !== 1) {
-    intMinutes--;
-    intTime.textContent = `${minTwoDidgets(intMinutes)}:${minTwoDidgets(intSeconds)}`;
+function minus(x) {
+  if (x.minutes !== 1) {
+    x.minutes--;
+    x.display.textContent = `${minTwoDidgets(x.minutes)}:${minTwoDidgets(x.seconds)}`;
   }
 }
 
-function intTimePlus() {
-  intMinutes++;
-  intTime.textContent = `${minTwoDidgets(intMinutes)}:${minTwoDidgets(intSeconds)}`;
+function plus(x) {
+  x.minutes++;
+  x.display.textContent = `${minTwoDidgets(x.minutes)}:${minTwoDidgets(x.seconds)}`;
 }
 
-//break timings
-function breakMinus() {
-  if (breakMinutes !== 1) {
-    breakMinutes--;
-    breakTime.textContent = `${minTwoDidgets(breakMinutes)}:${minTwoDidgets(breakSeconds)}`;
-  }
-}
-
-function breakPlus() {
-  breakMinutes++;
-  breakTime.textContent = `${minTwoDidgets(breakMinutes)}:${minTwoDidgets(breakSeconds)}`;
-}
-
-function timer(minutes, timer, bar, interval) {
-  let minuteCount = minutes,
+//timer function
+function timer(x) {
+  let minuteCount = x.minutes,
     secondsCount = 0,
-    totalTime = minutes * 60,
+    totalTime = x.minutes * 60,
     progress = 0;
-  bar.max = totalTime;
+  x.bar.max = totalTime;
   interval = setInterval(() => {
-    timer.textContent = `${minTwoDidgets(minuteCount)}:${minTwoDidgets(secondsCount)}`;
-    bar.value = progress;
+    x.display.textContent = `${minTwoDidgets(minuteCount)}:${minTwoDidgets(secondsCount)}`;
+    x.bar.value = progress;
     progress++;
     totalTime--;
     secondsCount--;
@@ -97,19 +80,22 @@ function timer(minutes, timer, bar, interval) {
   }, 1000);
 }
 
+function callTimer() {
+  if (timerCount % 2 === 0) {
+    timer(breakTimer);
+    timerCount++;
+  } else {
+    timer(sessionTimer);
+    timerCount--;
+  }
+}
+
+function toggleBtns() {
+  allBtns.forEach(x => x.classList.toggle('is-hidden'));
+}
+
 function minTwoDidgets(num) {
   let x = String(num);
   x.length < 2 ? x = `0${num}` : false;
   return x;
-}
-
-
-function callTimer() {
-  if (timerCount % 2 === 0) {
-    timer(breakMinutes, breakTime, breakBar, interval);
-    timerCount++;
-  } else {
-    timer(intMinutes, intTime, intervalBar, interval);
-    timerCount--;
-  }
 }
